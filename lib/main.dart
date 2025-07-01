148a672523e55b8ac3035a1a5714086c8d15b3c8
Ferdinando <ferdypaparo03@gmail.com>
2025-07-01 20:49:00 +0200

    implementazione navigazione tra le schermate principali

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'providers/vinile_provider.dart';
 import 'providers/categoria_provider.dart';
 import 'providers/ricerca_provider.dart';
 import 'providers/statistiche_provider.dart';
-/*import 'screens/schermata_principale.dart';
+
+import 'screens/schermata_principale.dart';
 import 'screens/schermata_categorie.dart';
-import 'screens/BarraSopra.dart';*/
+import 'screens/schermata_profilo.dart';
 
 void main() {
   runApp(
@@ -28,7 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Vinyl Collection',
+      title: 'VinylVault',
       debugShowCheckedModeBanner: false,
       home: const VistaTabs(title: 'VinylVault'),
     );
@@ -45,56 +47,72 @@ class VistaTabs extends StatefulWidget {
 }
 
 class _VistaTabsState extends State<VistaTabs> {
-  List<int> list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  int _selectedIndex = 0;
+
+  static const List<Widget> _screens = [
+    SchermataPrincipale(),
+    SchermataCategorie(),
+    SchermataProfilo(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() => _selectedIndex = index);
+  }
+
+  static const List<BottomNavigationBarItem> _navItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+    BottomNavigationBarItem(icon: Icon(Icons.library_add), label: 'Categorie'),
+    BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profilo'),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            /*scrolledUnderElevation: 4,*/
-            title: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/VinylVault.png',
-                  height: 50,
-                  width: 50,
-                ),
-                Text(widget.title),
-              ],
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/VinylVault.png',
+              height: 50,
+              width: 50,
             ),
-            titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-            backgroundColor: const Color(0xFF001237),
-
-            bottom: const TabBar(
-              indicatorColor: Colors.white,
-              labelColor: Colors.white,
-              unselectedLabelColor: Colors.white38,
-              tabs: [
-                Tab(icon: Icon(Icons.home), text: "Home",),
-                Tab(icon: Icon(Icons.library_add), text: "Catalogo",),
-                Tab(icon: Icon(Icons.person), text: "Profilo",),
-              ],
-            ),
-          ),
-
-          /*drawer: const Drawer(
-            child: ContenutoDelDrawer(),
-          ),
-
-          body: const TabBarView(
-              children: [
-                MyHomePage(title: "Home"),
-                /*Icon(Icons.home),*/
-                SchermataLista(),
-                Icon(Icons.favorite),
-                Icon(Icons.person),
-              ]
-          ),*/
-          /*floatingActionButton: ,*/
+            Text(widget.title),
+            const SizedBox(width: 10),
+          ],
         ),
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        backgroundColor: const Color(0xFF000B23),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search_rounded, color: Colors.white, size: 25),
+            tooltip: 'Ricerca',
+            onPressed: () {
+
+            },
+          ),
+        ],
+      ),
+
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _screens,
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: _navItems,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: const Color(0xFF000B23),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white54,
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 }
