761b331fa1e1f138647cee54d4843c8b9838ed30
Ferdinando <ferdypaparo03@gmail.com>
2025-07-06 11:53:43 +0200

    fix vari 3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:vinyl_collection_app/screens/schermata_principale.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SchermataPrincipale());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
